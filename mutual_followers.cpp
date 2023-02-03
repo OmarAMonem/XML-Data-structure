6193679ae68bd49dba148e25b22a2703efed4cbf
HadyElMaageny <87091270+HadyElMaageny@users.noreply.github.com>
2023-02-03 05:43:16 +0200

    Update mutual_followers.cpp

--- mutual_followers.cpp
+++ mutual_followers.cpp
@@ -7,7 +7,7 @@ using namespace std;
 vector<int> mutual_followers(int user1Id, int user2Id, vector<vector<int>> adjacencyMatrix)
 {
 	vector <int> result;
-	// We loop over the rows numbers that equal users’ ID parameters – 1.
+	// We loop over the rows numbers that equal usersâ€™ ID parameters â€“ 1.
 	for (int i = 0; i < adjacencyMatrix[user1Id].size(); i++)
 	{
 		// If the two users (rows) have Ones in the same column, 
@@ -18,27 +18,6 @@ vector<int> mutual_followers(int user1Id, int user2Id, vector<vector<int>> adjac
 			result.push_back(i);
 		}
 	}
-	// return the vector which contains the number of columns that represent the IDs of mutual followers – 1.
+	// return the vector which contains the number of columns that represent the IDs of mutual followers â€“ 1.
 	return result;
 }
-
-//int main()
-//{
-//	vector<vector<int>> adjacencyMatrix = { {0, 1, 1, 0},
-//											{1, 0, 0, 0},
-//											{1, 0, 0, 0},
-//											{1, 1, 1, 0} };
-//	vector <int> result = mutual_followers(1, 4, adjacencyMatrix);
-//	if (result.size() != 0)
-//	{
-//		cout << "Mutual Friends for users of id 1 & 4 are : " << endl;
-//		for (int i : result)
-//		{
-//			cout << "User of Id : " << i + 1 << endl;
-//		}
-//	}
-//	else
-//		cout << "There isn't mutual followers." << endl;
-//
-//	return 0;
-//}
